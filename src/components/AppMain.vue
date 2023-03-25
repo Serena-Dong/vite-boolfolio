f485dc3a40f37d3ba10171438858783afaa99346
Serena-Dong <serenadong3159@icloud.com>
2023-03-25 01:20:12 +0100

    project title on the page

---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from '../data/store.js'
+export default {
+    name: 'AppMain',
+    data: () => ({
+        store
+    })
+};
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <h3 v-for="project in store.projects">{{ project.title }}
+            </h3>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    background-color: rgb(40, 40, 40);
+}
+</style>
