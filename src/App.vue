61ab050f644f74fa127844a2ea6dc491f04ca368
Serena-Dong <serenadong3159@icloud.com>
2023-03-25 01:18:57 +0100

    call API

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
 import axios from 'axios';
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue';
+import { store } from './data/store.js';
+
+const apiBaseUri = 'http://127.0.0.1:8001/api';
+
+export default {
+  name: 'App',
+  components: { AppHeader, AppMain },
+  data: () => ({
+    store,
+  }),
+  methods: {
+    fetchProject() {
+      axios.get(apiBaseUri + '/projects').then(res => {
+        store.projects = res.data;
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  },
+  created() {
+    this.fetchProject();
+  }
+}
+
+
 </script>
 
 <template>
-  <div>
-
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
 </template>
 
 <style lang="scss">
