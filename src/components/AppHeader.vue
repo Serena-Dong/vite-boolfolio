aadd6f0eedfef793f1bd866c91cbaeee09fa5c27
Serena-Dong <serenadong3159@icloud.com>
2023-03-25 01:19:47 +0100

    title In the header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'AppHeader'
+};
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="navbar">
+                <h1>My Projects</h1>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    height: 10vh;
+    background-color: rgb(24, 24, 24);
+
+    display: flex;
+    align-items: center;
+}
+</style>
